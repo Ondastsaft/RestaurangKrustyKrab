916d3e2aa3efcfefa515490b33bba62f8b15a922
Ondastsaft <kenneth.jansson@gmail.com>
2022-11-03 11:36:24 +0100

    Initialise from Restaurangen V2

---
+++ RestaurantKrustyKrab/People/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace RestaurantKrustyKrab.People
+{
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public Person(string Name)
+        {
+            Name = this.Name;
+        }
+    }
+}
+
