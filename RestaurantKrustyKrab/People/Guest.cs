e7b88480e41013aa4c4a4be2f8efec08b62d30ad
KaveCoding <teachereli@outlook.com>
2022-11-07 20:22:42 +0100

    Prefered dish added

--- RestaurantKrustyKrab/People/Guest.cs
+++ RestaurantKrustyKrab/People/Guest.cs
@@ -1,10 +1,13 @@
-﻿namespace RestaurantKrustyKrab.People
+﻿using RestaurantKrustyKrab.Restaurant;
+namespace RestaurantKrustyKrab.People
 {
     internal class Guest : Person
     {
+        internal Dish Prefered_Dish { get; set; }
         public Guest(string name, int PositionX, int PositionY) : base(name, PositionX, PositionY)
         {
             Name = name;
+            Prefered_Dish = new Dish();
 
         }
     }
